af3132eab5d6b3ebe2aacdd2ac5dd70b266e393a
Julia Connelly <juliakconnelly@gmail.com>
2016-09-13 10:20:56 +0200

    added game manager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public Button startButton;
+
+    int currentSceneIndex;
+    static GameManager instance;
+
+    void Start() {
+        currentSceneIndex = 0;
+        instance = this;
+
+        startButton.onClick.AddListener(() => LoadNextScene());
+    }
+
+    public static void LoadNextScene() {
+        instance.currentSceneIndex++;
+        if (instance.currentSceneIndex >= SceneManager.sceneCount - 1) {
+            Debug.Log("No more scenes");
+        }else {
+            SceneManager.LoadScene(instance.currentSceneIndex);
+        }
+    }
+}
